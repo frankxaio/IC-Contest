4648c4cf0c539c34e1bc71d70ae13a31d37236ac
frankxaio <frankxaio@gmail.com>
2025-05-07 11:36:33 +0800

    folder name changes

---
+++ 1998_grad/desgin/test_bench.v
@@ -0,0 +1,95 @@
+/********************************************************
+*                     Test bench 			*
+*********************************************************
+*  1. The test bench files contain four files :         *
+*       test_bench.v  	-->  this file 			*
+*       INPUT.DAT	-->  input data sequence	*
+*       EXPECT.DAT	-->  expected output sequence	*
+*       in_file 	-->  simulation control file	*
+*  2. Output report generated :				*
+*	SIM.out		-->  your simulation result	*
+*  3. You need to modify one thing before simulation :  *
+*       <clock> : clock rate, e.g., 20 means 20MHz.	*
+********************************************************/
+
+`timescale 1ns/10ps
+module test_bench;
+wire [15:0] out;
+wire ready;
+reg  [7:0] in;
+reg  start, clock;
+reg  [7:0] rom [0:2999];
+reg [15:0] expt[0:2999];
+integer rom_addr, expt_addr;
+integer file_hdl;
+
+
+
+
+// =====  Modify this parameter to set the clock rate  ====
+
+   parameter CLOCK_RATE = <clock>;           // MHz
+
+// ========================================================
+
+
+
+
+parameter CYCLE = 1000/CLOCK_RATE; 
+  
+CHIP top(out, ready, in, start, clock);
+
+   initial 
+      begin
+         $VTOOLS_decal("in_file", "product.xch");
+         file_hdl = $fopen("SIM.OUT");
+         if(file_hdl == 0)
+            begin
+               $display("ERROR: Failed to open SIM.OUT!!!\n");
+               $finish;
+            end
+         clock = 1'b1;
+         start = 1'b0;
+         $readmemh("INPUT.DAT", rom);
+ 	 $readmemh("EXPECT.DAT", expt);
+         #(10.5*CYCLE) start = 1'b1;
+	 for(rom_addr=0;rom_addr<3000;rom_addr=rom_addr+1)
+            begin
+               @(posedge clock);
+               #(0.1*CYCLE) in = rom[rom_addr]; 
+            end
+	 #(0.5*CYCLE) start = 1'b0;
+         wait (ready == 1'b0);
+         #(3*CYCLE) 
+         $fclose(file_hdl);
+         $finish;
+      end
+
+   initial
+      begin
+      $fdisplay(file_hdl, "======================================");
+      $fdisplay(file_hdl, "Simulation clock rate = %dMHz", CLOCK_RATE);
+      $fdisplay(file_hdl, "======================================\n");
+      wait (ready == 1'b1);
+      @(negedge clock);
+      for(expt_addr=0;expt_addr<3000;expt_addr=expt_addr+1)
+         begin
+            if(out == expt[expt_addr])
+               $fdisplay(file_hdl, "         in %h  out %h  expt %h", in, out, expt[expt_addr]);
+            else
+               $fdisplay(file_hdl, "ERROR!!! in %h  out %h  expt %h", in, out, expt[expt_addr]);
+     	    @(negedge clock);	
+         end
+      end
+
+   always #(0.5*CYCLE) clock = ~clock;
+
+/*
+   initial
+      begin
+         $shm_open("waves.shm");
+         $shm_probe("AS");
+      end
+*/
+
+endmodule
--- 2019_univ/README.md
+++ 2019_univ/README.md
@@ -1,10 +1,8 @@
 # Image Convolutional Circuit Design
 
 ```YAML 
-last edited: 2025/05/05
+last edited: 2025/05/06
 ```
-
-
 
 ## File Tree
 
@@ -26,10 +24,6 @@ DESIGN/
 └── tsmc13_neg.v        # TSMC 13nm lib
 ```
 
-
-
-
-
 ## Algorithm
 
 處理順序為Convolutional(Layer 0) -> Max-pooling(Layer 1)等共2層的運算處理流程
@@ -40,11 +34,7 @@ DESIGN/
 
 >    Input($64\times64$ pixels) -> zero-padding -> conv kernel($3\times3$) -> ，故其結果為1 張尺寸為64(寬)x64(高) pixels 的圖。 Layer  1 要進行最大池化(max-pooling)運算，須採用2x2 的max-pooling 視窗及步幅(stride)為2 的規格進行。故結果將呈現1 張尺寸為32(寬) x 32(高) pixels 的圖。 最後將Layer 0 和Layer 1 的結果圖依題目規定寫到 testfixture 的內建記憶體後即可完成。
 
-
-
 ## Design
-
-
 
 ### Internal Signal
 
